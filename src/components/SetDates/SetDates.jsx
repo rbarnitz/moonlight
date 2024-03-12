c5b6b052195c5d8951a462bd53acfb23897f8fe9
Richard Barnitz <richardbarnitz@gmail.com>
2024-03-11 21:49:34 -0700

    errors using react date range picker, moon data calendar working

--- src/components/SetDates/SetDates.jsx
+++ src/components/SetDates/SetDates.jsx
@@ -4,11 +4,32 @@ import { useDispatch } from 'react-redux';
 import './SetDates.css';
 
 import Button from '@mui/material/Button';
+//import DateRange from 'react-day-picker/es/DateRange';
+import DayPicker from 'react-day-picker';
 
 const SetDates = () => {
   //linking to MoonData page
   let history = useHistory();
 
+  //set range state
+  const [range, setRange] = useState();
+
+  let footer = <p>Pick a date!</p>;
+  if (range?.from) {
+    if (!range.to) {
+      footer = <p>{format(range.from, 'PPP')}</p>;
+    } else if (range.to) {
+      footer = (
+        <>
+          <p>Selected dates:</p>
+          <p>
+            {format(range.from, 'PPP')}–{format(range.to, 'PPP')}
+          </p>
+        </>
+      );
+      console.log('date range is:', footer);
+    }
+  }
   //navigate to Calendar
   function handleNext() {
     history.push('/mytrips');
@@ -17,6 +38,17 @@ const SetDates = () => {
   return (
     <div>
       <h1>Set Dates</h1>
+
+      <br></br>
+      <DayPicker
+        mode="range"
+        min={2}
+        max={6}
+        selected={range}
+        onSelect={setRange}
+        numberOfMonths={2}
+      />
+
       <Button onClick={handleNext} variant="outlined">
         Next
       </Button>
