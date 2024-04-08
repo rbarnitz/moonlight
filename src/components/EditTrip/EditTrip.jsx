d00f46d8b16b9b12b8a77c6ae14d6fea2a55b4d5
Richard Barnitz <richardbarnitz@gmail.com>
2024-04-07 19:42:53 -0700

    edit function working and navigatin to mytrips

--- src/components/EditTrip/EditTrip.jsx
+++ src/components/EditTrip/EditTrip.jsx
@@ -1,25 +1,91 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import axios from 'axios';
 
 import {
   Button,
   Box,
+  Card,
+  CardContent,
+  CardActions,
   InputAdornment,
   TextField,
   IconButton,
+  Typography,
+  Paper,
+  Stack,
 } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import { DateTime } from 'luxon';
+
+import { format } from 'date-fns';
+import { DateTime, Interval } from 'luxon';
 import { DayPicker } from 'react-day-picker';
 import './EditTrip.css';
 
-function EditTrip() {
+function EditTrip(tripInfo) {
   const { id } = useParams();
   const [trip, setTrip] = useState(null);
+  const [tripData, setTripData] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  //location states
+  const [townName, setTownName] = useState('');
+  const [locationInfo, setLocationInfo] = useState('');
+  const [searchSuccess, setSearchSuccess] = useState(false);
+  const [resultName, setResultName] = useState('');
+  const [coordinates, setCoordinates] = useState(null);
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+  const [searchedLocation, setSearchedLocation] = useState();
+  const [timezone, setTimezone] = useState();
+
+  //setting search changes
+  const handleInputChange = (event) => {
+    setTownName(event.target.value);
+  };
+
+  //set up async function for API testing
+  const handleSearch = async () => {
+    console.log('search for: ', townName);
+
+    //API call
+    try {
+      const response = await fetch(
+        `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(
+          townName
+        )}&key=c5bf905a37154ab79398510f969d8f54`
+      );
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch API result');
+      }
+
+      const APIResponse = await response.json();
+
+      //declare result name:
+      let searchedLocation = APIResponse.results[0].formatted;
+
+      //declaring latitude & longitude data:
+      let lat = APIResponse.results[0].geometry.lat;
+      let lng = APIResponse.results[0].geometry.lng;
+
+      //declaring timezone data & retrieving offset time timezone info
+      let timezoneSearch =
+        APIResponse.results[0].annotations.timezone.offset_string;
+
+      //display next button
+      setSearchSuccess(true);
+      setResultName(searchedLocation);
+      setTimezone(timezoneSearch);
+      setLongitude(lat);
+      setLatitude(lng);
+    } catch (error) {
+      console.error(error.message);
+      setCoordinates(null);
+    }
+  };
 
   // State for the date range
   const [range, setRange] = useState({
@@ -83,16 +149,17 @@ function EditTrip() {
     const tripData = {
       trip_id: id,
       user_id: user,
-      trip_location: 'test place',
-      trip_latitude: 11.1111,
-      trip_longitude: 11.111,
-      timezone: 'test zone',
+      trip_location: resultName,
+      trip_latitude: longitude,
+      trip_longitude: latitude,
+      timezone: timezone,
       trip_start: startDate,
       trip_end: endDate,
     };
 
     console.log('Editing trip:', tripData);
     dispatch({ type: 'EDIT_TRIP', payload: tripData });
+    history.push(`/mytrips/${user}`);
   }
 
   //format shown dates
@@ -101,6 +168,12 @@ function EditTrip() {
     return dateTime.toFormat('MM/dd/yyyy');
   }
 
+  const handleEnter = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -108,25 +181,39 @@ function EditTrip() {
   return (
     <div>
       <div className="container">
-        <Box className="center" width="600px" p={2}>
-          <TextField
-            id="outlined-basic"
-            label="Enter Location"
-            variant="outlined"
-            type="text"
-            color="success"
-            defaultValue={trip.trip_location}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton aria-label="search" edge="end">
-                    <SearchIcon />
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Box>
+        <Stack>
+          <Box className="center" width="600px" p={2}>
+            <TextField
+              id="outlined-basic"
+              label={trip.trip_location}
+              variant="outlined"
+              type="text"
+              onChange={handleInputChange}
+              color="success"
+              onKeyPress={handleEnter}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={handleSearch}
+                      aria-label="search"
+                      edge="end"
+                    >
+                      {' '}
+                      <SearchIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Box>
+          {searchSuccess && (
+            <Box p={2}>
+              <p>New location: {resultName}</p>
+            </Box>
+          )}
+        </Stack>
+
         <DayPicker
           mode="range"
           min={2}
